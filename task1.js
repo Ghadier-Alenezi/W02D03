1e08a30517798b363d391a791cc2fd8d86d8866d
Ghadier-Alenezi <Ghadier.Mutar.elq.js@tuwaiq.edu.sa>
2021-10-19 21:06:04 +0300

    task1 first update

---
+++ task1.js
@@ -0,0 +1,30 @@
+/* 
+Change an image when the mouse moves onto it,
+and then back to the original image when the mouse moves off of it.
+*/
+function changeImg() {
+  document.getElementById("img1").src = "./programming.png";
+}
+function returnImg() {
+  document.getElementById("img1").src = "./tuwaiq.png";
+}
+
+/*
+Create a script which creates an alert on the page 
+when a user right clicks on a specific image.
+*/
+
+/*
+ Create an alert on the page when a user enters data 
+ into a input.
+*/
+
+/*
+Create an alert to let the user know when 
+the page has loaded
+*/
+
+/*
+Highlight all paragraphs when the mouse moves over them 
+(then revert back once it has moved past them).
+*/
